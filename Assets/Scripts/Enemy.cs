c733f21ae2fe9cc407b0b36394afbc3bb93797f6
intr0vert <intr0vert.noname@mail.ru>
2020-02-29 00:39:10 +0300

    Added obstacle swapner and moving

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	public int damage = 1;
+	public float speed;
+
+	private void Update()
+	{
+		transform.Translate(Vector2.left * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player")) {
+			// Player takes damage!
+			other.GetComponent<Player>().health -= damage;
+			Destroy(gameObject);
+		}
+	}
+
+}
